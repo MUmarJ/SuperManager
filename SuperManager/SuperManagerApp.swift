03b23e3f85d5d321f0a5fca0027eb77a5db9a9da
MUmarJ <umarjh96@gmail.com>
2023-01-31 20:44:48 -0600

    Initial Commit

---
+++ SuperManager/SuperManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperManagerApp.swift
+//  SuperManager
+//
+//  Created by MUmarJ on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
