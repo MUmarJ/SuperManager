5095a7ad577e14be0aa268ea19b33395fee7f4dd
MUmarJ <umarjh96@gmail.com>
2023-02-20 15:52:56 -0600

    Create HomeView

--- SuperManager/HomeView.swift
+++ SuperManager/HomeView.swift
@@ -5,4 +5,28 @@
 //  Created by MUmarJ on 2/1/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    let cards: [HeroCard]
+    var body: some View {
+        VStack {
+            CarouselView(cards: cards)
+            List {
+                ForEach(1..<4) { _ in
+                    CardView()
+
+                }
+            }
+        }
+            .padding(.horizontal)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(cards:
+            HeroCard.samples
+        )
+    }
+}
