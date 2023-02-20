8aefbc9d614c32a1b9ba44ce89d5470cf7f72d2c
MUmarJ <umarjh96@gmail.com>
2023-02-20 15:52:43 -0600

    Create CardView

--- SuperManager/CardView.swift
+++ SuperManager/CardView.swift
@@ -5,4 +5,43 @@
 //  Created by MUmarJ on 2/1/23.
 //
 
-import Foundation
+//
+//  CardView.swift
+//  Scrumy
+//
+//  Created by MUmarJ on 1/30/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Title Words")
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
+            Spacer()
+            HStack {
+                Label("5", systemImage: "person.3")
+
+                Spacer()
+                Label("10", systemImage: "clock")
+
+                    .labelStyle(.trailingIcon)
+            }
+                .font(.caption)
+        }
+            .padding()
+            .foregroundColor(.black)
+    }
+}
+struct CardView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        CardView()
+            .background(.cyan)
+            .previewLayout(
+                .fixed(width: 400, height: 60)
+        )
+    }
+}
